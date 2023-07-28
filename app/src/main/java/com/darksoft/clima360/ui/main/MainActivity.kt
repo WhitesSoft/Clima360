d3e0e6995df5ed966196b85e8de200411df27fc3
WhitesSoft <juan.moonteroo@gmail.com>
2023-07-27 23:30:06 -0400

    Julio 27 | design main activity | Juan Montero

--- app/src/main/java/com/darksoft/clima360/ui/main/MainActivity.kt
+++ app/src/main/java/com/darksoft/clima360/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
-package com.darksoft.clima360.ui
+package com.darksoft.clima360.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.darksoft.clima360.R
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

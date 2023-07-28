e4ebb3caa35e8fa45027f374bd913c6a10e791f6
WhitesSoft <juan.moonteroo@gmail.com>
2023-07-27 21:48:15 -0400

    primer commit

---
+++ app/src/main/java/com/darksoft/clima360/ui/Clima360App.kt
@@ -0,0 +1,8 @@
+package com.darksoft.clima360.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Clima360App: Application() {
+}
---
+++ app/src/main/java/com/darksoft/clima360/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.darksoft.clima360.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.darksoft.clima360.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
